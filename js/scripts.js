6a1b8f9189b4589302d48a5a9db4f137d2a5d50a
jumpman22 <hiram.cho@gmail.com>
2018-06-07 13:18:47 -0700

    complete order

--- js/scripts.js
+++
@@ -1,8 +0,0 @@
-//DISPLAY TEXT BOX AFTER OTHER IS SELECTED IN DROPDOWN
-function checkvalue(val) {
-    if (val === "other")
-        document.getElementById('addressType').style.display = 'block';
-    else
-        document.getElementById('addressType').style.display = 'none';
-}
-
